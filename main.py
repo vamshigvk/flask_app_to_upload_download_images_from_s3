9fece6dadcc084f590b46d9ba9438ff463f9c303
Your Name <vamshikrishnagvk5@gmail.com>
2022-01-07 22:47:47 +0530

    working image and json

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from werkzeug.utils import secure_filename
 import boto3
 
 s3 = boto3.client('s3',
-                    aws_access_key_id='keyid' ,
-                    aws_secret_access_key= 'secretkey' #,aws_session_token=
+                    aws_access_key_id=':)' ,
+                    aws_secret_access_key= ':P' #,aws_session_token=
                      )
 
 BUCKET_NAME='data.science.python.projects'
@@ -15,46 +15,44 @@ BUCKET_NAME='data.science.python.projects'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'tiff'])
 
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
 def upload_form():
-	return render_template('upload.html')
+    return render_template('upload.html')
 
 @app.route('/', methods=['POST'])
 def upload_image():
-	if 'file' not in request.files:
-		flash('No file part')
-		return redirect(request.url)
-	file = request.files['file']
-	if file.filename == '':
-		flash('No image selected for uploading')
-		return redirect(request.url)
-	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		s3.upload_file(Bucket = BUCKET_NAME, Filename='static/uploads/'+filename, Key = 'textract/'+filename)
-		print('upload_image filename: ' + filename)
-		flash('Image successfully uploaded and displayed below')
-		time.sleep(5)
-		filename_json = filename.split('.')[0] +'.json'
-		print('jsonfile name is',filename_json)
-
-		s3.download_file(BUCKET_NAME, 'object_json/'+filename_json, 'static/downloads/'+filename_json)
-
-		return render_template('upload.html', filename=filename)
-	else:
-		flash('Allowed image types are -> png, jpg, jpeg, gif')
-		return redirect(request.url)
-
-@app.route('/display/<filename>')
-def display_image(filename):
-	#print('display_image filename: ' + filename)
-    with open('static/downloads/ocr3.json', 'r') as myfile:
-        data = myfile.read()
-
-    return redirect(url_for('static', filename='uploads/' + filename, jsonfile = json.dumps(data)), code=301)
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        s3.upload_file(Bucket = BUCKET_NAME, Filename='static/uploads/'+filename, Key = 'textract/'+filename)
+        #s3.upload_file(Bucket = BUCKET_NAME, Filename='static/uploads/'+filename, Key = 'textract_expenses/'+filename)
+        print('upload_image filename: ' + filename)
+        flash('Image successfully uploaded and displayed below')
+        time.sleep(7)
+        filename_json = filename.split('.')[0] +'.json'
+        print('jsonfile name is',filename_json)
+        try:
+            s3.download_file(BUCKET_NAME, 'object_json/'+filename_json, 'static/downloads/'+filename_json)
+        except:
+            flash('Failed to load json file, try again')
+            return redirect(request.url)
+        filename_json = filename.split('.')[0] +'.json'
+        with open('static/downloads/'+filename_json, 'r') as myfile:
+            data = myfile.read()
+        return render_template('upload.html', filename=filename, data=data)
+    else:
+        flash('Allowed image types are -> png, jpg, jpeg, gif')
+        return redirect(request.url)
 
 
 if __name__ == "__main__":
